98753e50087957cb7965efc74a3a76e72a5febdf
Anjar Harimurti <anjarharimurti.dev@gmail.com>
2024-05-03 15:50:18 +0700

    Initial Commit

---
+++ MiniKita/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniKita
+//
+//  Created by Anjar Harimurti on 03/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniKita/MiniKitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniKitaApp.swift
+//  MiniKita
+//
+//  Created by Anjar Harimurti on 03/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MiniKitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
