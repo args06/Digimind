cb732e05d04aee722b966e89af7aa298e69aea67
estergbriel <esther_g20@mhs.istts.ac.id>
2024-05-20 17:27:17 +0700

    style(splash): Create Splash Screen

---
+++ MiniKita/View/Splash/SplashScreen.swift
@@ -0,0 +1,40 @@
+//
+//  SplashScreen.swift
+//  MiniKita
+//
+//  Created by Esther Gabriel Trivena on 20/05/24.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+    
+    @State var isActive = false
+    
+    var body: some View {
+        VStack {
+            if isActive {
+                ContentView()
+            } else {
+                VStack {
+                    Image("splash")
+                        .resizable()
+                        .frame(width: 190, height: 144)
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color("JungleGreen"))
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    isActive = true
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreen()
+}
